bbe60951f5dd990a57d60ced979cc4a249cf6354
Code_Father <rauf.raymond110@gmail.com>
2020-07-01 00:21:45 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const nameInput= document.getElementById("name");
+const numberInput= document.getElementById("number");
+const messageInput= document.getElementById("message");
+function zoomImg(){
+    let img=document.getElementById("tv-image");
+    img.classList.add("zoom-in-tv");
+}
+window.addEventListener('scroll', function(e) {
+    if(e.path[1].scrollY >330){
+        zoomImg();
+    }
+});
+window.onload = function() {
+    document.getElementById("submit-btn").addEventListener('click',(event)=>{
+
+        let message = {
+            name: nameInput.value,
+            number: numberInput.value,
+            message: messageInput.value
+        };
+        emailjs.send("gmail", "artkom", message,'user_cQXScjsh5GkmXVxqDhC9F')
+    });
+    const loaderContainer= document.getElementsByClassName("loader-container")[0];
+    loaderContainer.classList.add("hidden");
+};
+
+
+
