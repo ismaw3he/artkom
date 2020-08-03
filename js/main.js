023791912d6526203826ac8fd1bf4aae3f9ae5be
Rauf Ismayilov <rauf.raymond110@gmail.com>
2020-08-03 07:09:07 +0400

    added slider

--- js/main.js
+++ js/main.js
@@ -24,5 +24,81 @@ window.onload = function() {
     loaderContainer.classList.add("hidden");
 };
 
+const slider = document.querySelector(".slider");
+
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
+const indicatorParents = document.querySelector('.controls ul');
+let sectionIndex = 0;
+
+function setIndex() {
+    document.querySelector(".controls .selected")
+        .classList.remove("selected");
+    slider.style.transform = "translate(" + (sectionIndex) * -33.33 + "%)";
+}
+
+function moveSlider(direction) {
+    if (direction === "left") {
+        sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+        setIndex()
+        indicatorParents.children[sectionIndex].classList.add('selected');
+    }
+    else if (direction === "right") {
+        sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
+        setIndex()
+        indicatorParents.children[sectionIndex].classList.add('selected');
+    }
+}
+
+document.querySelectorAll(".controls li").forEach(function (indicator, ind) {
+    indicator.addEventListener("click", () => {
+        sectionIndex = ind;
+        setIndex()
+        indicator.classList.add("selected")
+
+    });
+});
+
+leftArrow.addEventListener("click", () => {
+    moveSlider("left");
+});
+
+rightArrow.addEventListener("click", () => {
+    moveSlider("right");
+});
+
+const carousel = document.querySelector(".carousel");
+let initialPosition = null;
+let moving = false;
+
+function gestureDown(e) {
+    initialPosition = e.pageX;
+    moving = true;
+}
+function gestureMove(e) {
+    if (moving) {
+        const currentPosition = e.pageX;
+        const diff = currentPosition - initialPosition;
+        if (diff < 0) {
+            moveSlider("right");
+        }
+        else {
+            moveSlider("left");
+        }
+        moving = false;
+    }
+}
+
+if (window.PointerEvent) {
+    slider.addEventListener("pointerdown", gestureDown)
+    slider.addEventListener("pointermove", gestureMove)
+}
+else {
+    slider.addEventListener("touchdown", gestureDown)
+    slider.addEventListener("touchmove", gestureMove)
+    slider.addEventListener("mousedown", gestureDown)
+    slider.addEventListener("mousemove", gestureMove)
+}
+
 
 
